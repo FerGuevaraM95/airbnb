68461f1e3c2ce8e70b925c4e6af755a555c50d5e
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-07-29 14:23:50 -0500

    Add transition to hero

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const imgHero = document.querySelector('.hero');
+
+    //Iniciar las variables
+    let i = 0;
+    let time = 0;
+
+    // Array de imagenes de fondo
+    const images = ['arriba2.jpg', 'arriba.jpg'];
+
+    setInterval( () => {
+        imgHero.style.backgroundPositionY = '-'+time+'px';
+
+        if(time > 40) {
+            time = 0;
+            imgHero.style.backgroundImage = "url(../img/"+images[i]+")";
+
+            if(i === images.length -1) {
+                i = 0;
+            } else {
+                i++;
+            }
+            console.log(images[i]);
+        }
+        time++;
+
+        console.log(time)
+    }, 100);
+
     // Boton float en el footer
     const btnFloat = document.querySelector('.btn-float');
     btnFloat.addEventListener('click', (e) => {
